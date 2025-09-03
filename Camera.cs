59fcad8d61393b9be2bb05ce25163bcc7788835d
Otto Leppänen <jkvo17@gmail.com>
2025-09-03 13:58:35 +0300

    haha

--- Camera.cs
+++ Camera.cs
@@ -5,9 +5,26 @@ public partial class Camera : Camera2D
 {
 	[Export] private float Speed = 100f;
 	private Vector2 velocity = Vector2.Up;
+	private Area2D _deathBox;
+
+	public override void _Ready()
+	{
+		_deathBox = GetNode<Area2D>("Deathbox");
+		_deathBox.BodyEntered += OnBodyEntered;
+	}
 
 	public override void _Process(double delta)
 	{
 		Position += velocity * Speed * (float)delta;
+	}
+
+	private void OnBodyEntered(Node body)
+	{
+		if (body is Player player)
+		{
+			GD.Print("👽👽👽");
+			player.QueueFree();
+			Speed = 0f;
+		}
 	}
 }
