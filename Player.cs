e2b7dd1b14e774881526b82fd032d5f550e41523
joe-vis <joel.visakorpi@gmail.com>
2025-09-03 13:59:21 +0300

    speed

--- Player.cs
+++ Player.cs
@@ -3,39 +3,50 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-    private float _speed = 10;
+    private float _speed = 600;
     private Vector2 _movementVector = Vector2.Zero;
-	public const float JumpVelocity = -400.0f;
+    public const float JumpVelocity = -600.0f;
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector2 velocity = Velocity;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        _movementVector = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+    }
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 velocity = Velocity;
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
+        // Add the gravity.
+        if (!IsOnFloor())
+        {
+            velocity += GetGravity() * 2 * (float)delta;
+        }
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * _speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, _speed);
-		}
+        // Handle Jump.
+        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+        {
+            velocity.Y = JumpVelocity;
+        }
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+        // Get the input direction and handle the movement/deceleration.
+        // As good practice, you should replace UI actions with custom gameplay actions.
+        // if (IsOnFloor())
+
+        if (_movementVector != Vector2.Zero)
+        {
+            velocity.X = _movementVector.X * _speed;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, _speed);
+        }
+
+        Velocity = velocity;
+        MoveAndSlide();
+    }
+
+    private void Jump()
+    {
+    }
 }
